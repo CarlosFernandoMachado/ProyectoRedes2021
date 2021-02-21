6c6107ec957c1e5263fec8d02f4926294cc79c32
Nahin Buezo <nahinbuezo@gmail.com>
2021-02-20 19:43:56 -0600

    deleeet

--- mercadito-online/backend/src/libs/boot.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    app.db.sequelize.sync();
-    app.listen(app.get('port'), () => {
-        console.log('Server on port', app.get('port'));
-    });
-  
-}
-
--- mercadito-online/backend/src/libs/middlewares.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-module.exports = app =>{
-    app.set('port', process.env.PORT || 3000);
-
-    //middlewares
-    app.use(express.json());
-}
