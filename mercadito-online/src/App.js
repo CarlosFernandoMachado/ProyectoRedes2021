049c441b68a7cafc232c3aad4c0d992648690e64
Nahin Buezo <nahinbuezo@gmail.com>
2021-02-20 18:04:17 -0600

    desarrolando backend
    Desarrollando backen, instalando sqlite3, sequelize y babel asi como algunas carpetasnecesarias

--- mercadito-online/src/App.js
+++ mercadito-online/src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
 import Navbar from "./Navbar/Navbar";
 
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
+
+callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+  render(){
+    return (
+      <div>
+        <Navbar />
+        <p className="App-intro">;{this.state.apiResponse}</p>
+
+      </div>
+    );
+  }
 }
 
 export default App;
