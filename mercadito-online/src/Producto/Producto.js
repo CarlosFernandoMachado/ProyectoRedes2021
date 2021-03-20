5c23abc3bec23c825e89a129afd7bba195b241f6
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2021-03-20 11:32:36 -0600

    Modificando productos

--- mercadito-online/src/Producto/Producto.js
+++ mercadito-online/src/Producto/Producto.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
@@ -8,7 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
-import {DataContext} from '../Context';
+import { DataContext } from '../Context';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
@@ -16,17 +16,17 @@ import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
 export class Producto extends Component {
   static contextType = DataContext;
   render() {
-    const {products,addCart, increase, decrease} = this.context;
+    const { products, addCart, increase, reduction, removeProduct } = this.context;
     console.log(products);
     function createData(id, name, cantidad, precio) {
-      return {id, name, cantidad, precio };
+      return { id, name, cantidad, precio };
     }
     const rows = [
-      createData(1,'Frozen yoghurt', 5, 50),
-      createData(2,'Ice cream sandwich', 10, 18),
-      createData(3,'Eclair', 2, 80),
-      createData(4,'Cupcake', 20, 8),
-      createData(5,'Gingerbread', 17, 12),
+      createData(1, 'Frozen yoghurt', 5, 50),
+      createData(2, 'Ice cream sandwich', 10, 18),
+      createData(3, 'Eclair', 2, 80),
+      createData(4, 'Cupcake', 20, 8),
+      createData(5, 'Gingerbread', 17, 12),
     ];
 
     const StyledTableCell = withStyles((theme) => ({
@@ -49,36 +49,42 @@ export class Producto extends Component {
 
     const StyledTable = withStyles((theme) => ({
       root: {
-          minWidth: 700,
+        minWidth: 700,
       },
     }))(Table);
 
     return (
       <div id="product">
-      <TableContainer component={Paper}>
-        <StyledTable>
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Nombre</StyledTableCell>
-              <StyledTableCell align="right">Cantidad</StyledTableCell>
-              <StyledTableCell align="right">Precio</StyledTableCell>
-              <StyledTableCell align="right">Producto a comprar</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product) => (
-              <StyledTableRow key={product.id}>
-                <StyledTableCell component="th" scope="row">{product.title}</StyledTableCell>
-                <StyledTableCell align="right">{product.cantidad}</StyledTableCell>
-                <StyledTableCell align="right">{product.precio}</StyledTableCell>
-                <StyledTableCell align="right">
-                  <Button variant="contained" color="primary" onClick={()=> addCart(product.id)}>Agregar al carrito</Button>
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </StyledTable>
-      </TableContainer>
+        <TableContainer component={Paper}>
+          <StyledTable>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Nombre</StyledTableCell>
+                <StyledTableCell align="right">Cantidad</StyledTableCell>
+                <StyledTableCell align="right">Precio</StyledTableCell>
+                <StyledTableCell align="right">Producto a comprar</StyledTableCell>
+                <StyledTableCell align="right">Eliminar</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product) => (
+                <StyledTableRow key={product.id}>
+                  <StyledTableCell component="th" scope="row">{product.title}</StyledTableCell>
+                  <StyledTableCell align="right">{product.cantidad}</StyledTableCell>
+                  <StyledTableCell align="right">{product.precio}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    <RemoveCircleIcon color="secondary" fontSize="small" onClick={() => reduction(product.id)} />
+                    {product.cantidad}
+                    <AddCircleIcon color="secondary" fontSize="small" onClick={() => addCart(product.id)} />
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                  <RemoveShoppingCartIcon color="secondary" fontSize="small" onClick={() => removeProduct(product.id)}/>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </StyledTable>
+        </TableContainer>
       </div>
     );
   }
